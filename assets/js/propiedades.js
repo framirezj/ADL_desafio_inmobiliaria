afdee86a682e61e2cf851a811676437a672f12c9
framirezj <12649259+framirezj@users.noreply.github.com>
2025-03-08 17:55:05 -0300

    created: render de las propiedades en venta y alquiler

--- assets/js/propiedades.js
+++ assets/js/propiedades.js
@@ -1,4 +1,4 @@
-alert("hoja conectada!!!!");
+//alert("hoja conectada!!!!");
 
 /*  Generar dos arreglos de propiedades (Uno para venta y otro para alquiler) que
  cumplan la siguiente estructura que se muestra de ejemplo. Una vez generados los
@@ -20,18 +20,9 @@ alert("hoja conectada!!!!");
  ]
   */
 
+//DATA
+
 const ventas = [
-  {
-    nombre: "Apartamento de lujo en zona exclusiva",
-    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-    descripcion:
-      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
-    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
-    habitaciones: 1,
-    costo: 5000,
-    smoke: false,
-    pets: true,
-  },
   {
     nombre: "Apartamento de lujo en zona exclusiva",
     src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
@@ -49,9 +40,20 @@ const ventas = [
     descripcion:
       "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 2,
+    costo: 5000,
+    smoke: true,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
     habitaciones: 3,
     costo: 5000,
-    smoke: false,
+    smoke: true,
     pets: true,
   },
   {
@@ -62,7 +64,7 @@ const ventas = [
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
     habitaciones: 4,
     costo: 5000,
-    smoke: false,
+    smoke: true,
     pets: true,
   },
 ];
@@ -70,89 +72,106 @@ const ventas = [
 const alquileres = [
   {
     nombre: "Apartamento de lujo en zona exclusiva",
-    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
     descripcion:
       "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
-    habitaciones: 4,
+    habitaciones: 2,
     costo: 5000,
     smoke: false,
     pets: true,
   },
   {
     nombre: "Apartamento de lujo en zona exclusiva",
-    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
     descripcion:
       "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
-    habitaciones: 4,
+    habitaciones: 2,
     costo: 5000,
-    smoke: false,
+    smoke: true,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 3,
+    costo: 5000,
+    smoke: true,
     pets: true,
   },
   {
     nombre: "Apartamento de lujo en zona exclusiva",
-    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
     descripcion:
       "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
     habitaciones: 4,
     costo: 5000,
-    smoke: false,
-    pets: true,
-  },
-  {
-    nombre: "Apartamento de lujo en zona exclusiva",
-    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-    descripcion:
-      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
-    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
-    habitaciones: 4,
-    costo: 5000,
-    smoke: false,
+    smoke: true,
     pets: true,
   },
 ];
 
 /* mostrar las propiedades */
 
-const rowVentas = document.querySelector("#row-ventas");
+const rowPropiedades = document.querySelector("#propiedades");
 
 let template = "";
+let propiedades = [];
 
-for (const propiedad of ventas) {
+const permitido_fumar = propiedades.smoke
+  ? `<p class="text-success">
+                  <i class="fas fa-smoking"></i> Permitido fumar
+                </p>`
+  : `<p class="text-danger">
+                  <i class="fas fa-smoking-ban"></i> No se permite fumar
+                </p>`;
+                
+const permitido_mascotas = propiedades.pets
+  ? `<p class="text-success">
+                  <i class="fas fa-dog"></i> Se permiten mascotas
+                </p>`
+  : `<p class="text-danger">
+                  <i class="fa-solid fa-ban"></i> No se permiten mascotas
+                </p>`;
 
+if (window.location.pathname === "/propiedades_venta.html") {
+  propiedades = ventas;
+} else if (window.location.pathname === "/propiedades_alquiler.html") {
+  propiedades = alquileres;
+}
+
+for (const propiedad of propiedades) {
   template += `
     <div class="col-md-4 mb-4">
             <div class="card">
               <img
-                src="https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg"
+                src="${propiedad.src}"
                 class="card-img-top"
                 alt="Imagen del departamento"
               />
               <div class="card-body">
                 <h5 class="card-title">
-                  Apartamento de lujo en zona exclusiva
+                  ${propiedad.nombre}
                 </h5>
                 <p class="card-text">
-                  Este apartamento de lujo está ubicado en una exclusiva zona
-                  residencial
+                  ${propiedad.descripcion}
                 </p>
                 <p>
-                  <i class="fas fa-map-marker-alt"></i> 123 Luxury Lane,
-                  Prestige Suburb, CA 45678
+                  <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
                 </p>
                 <p>
-                  <i class="fas fa-bed"></i> 4 Habitaciones |
+                  <i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |
                   <i class="fas fa-bath"></i> 4 Baños
                 </p>
-                <p><i class="fas fa-dollar-sign"></i> 5.000</p>
-                <p class="text-danger">
-                  <i class="fas fa-smoking-ban"></i> No se permite fumar
-                </p>
-                <p class="text-danger">
-                  <i class="fa-solid fa-ban"></i> No se permiten mascotas
-                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+
+                ${permitido_fumar}                
+                ${permitido_mascotas}
               </div>
             </div>
           </div>
@@ -160,41 +179,4 @@ for (const propiedad of ventas) {
   `;
 }
 
-rowVentas.innerHTML = template;
-
-/*
-
-<div class="col-md-4 mb-4">
-            <div class="card">
-              <img
-                src="https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg"
-                class="card-img-top"
-                alt="Imagen del departamento"
-              />
-              <div class="card-body">
-                <h5 class="card-title">
-                  Apartamento de lujo en zona exclusiva
-                </h5>
-                <p class="card-text">
-                  Este apartamento de lujo está ubicado en una exclusiva zona
-                  residencial
-                </p>
-                <p>
-                  <i class="fas fa-map-marker-alt"></i> 123 Luxury Lane,
-                  Prestige Suburb, CA 45678
-                </p>
-                <p>
-                  <i class="fas fa-bed"></i> 4 Habitaciones |
-                  <i class="fas fa-bath"></i> 4 Baños
-                </p>
-                <p><i class="fas fa-dollar-sign"></i> 5.000</p>
-                <p class="text-danger">
-                  <i class="fas fa-smoking-ban"></i> No se permite fumar
-                </p>
-                <p class="text-danger">
-                  <i class="fa-solid fa-ban"></i> No se permiten mascotas
-                </p>
-              </div>
-            </div>
-          </div>
- */
+rowPropiedades.innerHTML = template;
